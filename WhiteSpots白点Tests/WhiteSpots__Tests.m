a17a870d5c9d659df861a318ba98f5286daee61d
cjq002 <chenjiaqing@Mac-mini-CJQ.local>
2015-02-06 09:43:58 +0800

    Initial Commit

---
+++ WhiteSpots白点Tests/WhiteSpots__Tests.m
@@ -0,0 +1,34 @@
+//
+//  WhiteSpots__Tests.m
+//  WhiteSpots白点Tests
+//
+//  Created by 陈家庆 on 15-2-6.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WhiteSpots__Tests : XCTestCase
+
+@end
+
+@implementation WhiteSpots__Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
